cb4ed1d813b8a2a7c611ca92c1202dbbfa8ed543
MAKUSref <tomaszmakowski002731@gmail.com>
2025-12-06 17:43:07 +0100

    Add heading styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,16 +8,16 @@ export const HomePage = () => {
     <>
       <Navbar />
       <section className="flex flex-col items-center justify-center gap-15 mt-20 text-center">
-        <h1 className="font-bold text-8xl">
-          Uczestnicz w procesie tworzenia{" "}
+        <h1>
+          Uczestnicz w procesie tworzenia
           <span className="bg-red-400 inline-flex pb-3 px-2">prawa</span> w
           Polsce
         </h1>
 
-        <h4 className="text-4xl w-250">
+        <h5 className="max-w-[900px]">
           Dołącz do otwartego procesu konsultacji i miej realny wpływ na
           przepisy, które kształtują nasze codzienne życie.
-        </h4>
+        </h5>
         <button className="p-4 border-black border-2 rounded-4xl flex flex-row items-center gap-1 hover:cursor-pointer text-2xl">
           Przejrzyj dokumenty legislacyjne
         </button>
