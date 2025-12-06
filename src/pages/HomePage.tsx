d1e476a49e582c40bf496646d2fa2d81878f586b
Krzysiu <krzysiu@Krzysiu.dom>
2025-12-06 16:50:55 +0100

    Start preparing home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import { ConfirmModal } from "@/components/organisms/modals/ConfirmModal";
 import { useHealthCheck } from "@/hooks/useHealthCheck";
 import { Navbar } from "@/components/molecules/Navbar";
 
@@ -8,11 +7,21 @@ export const HomePage = () => {
   return (
     <>
       <Navbar />
-      <div className="">
-        <h1 className="">Welcome to the Home Page</h1>
-        <p className="">This is the main landing page of the application.</p>
-        <ConfirmModal />
-      </div>
+      <section className="flex flex-col items-center justify-center gap-15 mt-20 text-center">
+        <h1 className="font-bold text-8xl">
+          Uczestnicz w procesie tworzenia{" "}
+          <span className="bg-red-400 inline-flex pb-3 px-2">prawa</span> w
+          Polsce
+        </h1>
+
+        <h4 className="text-4xl w-250">
+          Dołącz do otwartego procesu konsultacji i miej realny wpływ na
+          przepisy, które kształtują nasze codzienne życie.
+        </h4>
+        <button className="p-4 border-black border-2 rounded-4xl flex flex-row items-center gap-1 hover:cursor-pointer text-2xl">
+          Przejrzyj dokumenty legislacyjne
+        </button>
+      </section>
     </>
   );
 };
