f9c24f195ba957bda86c40587e96924ac80a15ad
Krzysiu <krzysiu@Krzysiu.dom>
2025-12-06 16:21:28 +0100

    Add simple navbar component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,27 +1,18 @@
 import { ConfirmModal } from "@/components/organisms/modals/ConfirmModal";
 import { useHealthCheck } from "@/hooks/useHealthCheck";
-import { useAppSelector } from "@/redux/hooks";
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-import { PATHS } from "@/router/paths";
-
+import { Navbar } from "@/components/molecules/Navbar";
 
 export const HomePage = () => {
   useHealthCheck();
-  const accessToken = useAppSelector((state) => state.session.accessToken);
-  const navigate = useNavigate();
-  
-  useEffect(() => {
-    if (!accessToken) {
-      navigate(PATHS.LOGIN);
-    }
-  }, [accessToken, navigate])
 
   return (
-    <div className="">
-      <h1 className="">Welcome to the Home Page</h1>
-      <p className="">This is the main landing page of the application.</p>
-      <ConfirmModal />
-    </div>
+    <>
+      <Navbar />
+      <div className="">
+        <h1 className="">Welcome to the Home Page</h1>
+        <p className="">This is the main landing page of the application.</p>
+        <ConfirmModal />
+      </div>
+    </>
   );
 };
